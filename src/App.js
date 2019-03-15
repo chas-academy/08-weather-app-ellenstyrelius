eb5a45f29970d7de9bb9634fc8994178d4b0b87e
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-03-15 15:50:05 +0100

    Add file with API key stored in variable, add that file to .gitignore. In App.js, inside App class coponent: add fetchWeatherData function fetching data from DarkSky API, for now only console logging the result; add componentDidMount() from where fetchWeatherData is called.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import apiKey from './ApiKey';
+
+const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+const weatherDataUrl = `${proxyUrl}https://api.darksky.net/forecast/${apiKey}/37.8267,-122.4233`;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: null
+    }
+  }
+
+  fetchWeatherData() {
+    return fetch(weatherDataUrl)
+      .then(res => res.json())
+      .catch(error => console.log(error))
+      .then(res => {
+				console.log('🐐: App -> fetchWeatherData -> res', res)
+        return res;
+      })
+      .catch(error => console.log(error))
+  }
+  
+
+  componentDidMount() {
+    this.fetchWeatherData();
+  }
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>rain or shine</h1>
+          <h2>weather forecaster</h2>
         </header>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
