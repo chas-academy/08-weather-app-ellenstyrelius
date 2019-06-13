d75b488078f5eb4e7831631c981608aa4ebbe1bf
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-06-13 19:26:49 +0200

    Change format of date in WeatherWeek component and also remove variables with the temperatureLow value since it's apparently night time temperature and I don't think it's needed.

--- src/WeatherWeek.js
+++ src/WeatherWeek.js
@@ -2,30 +2,24 @@ import React from 'react';
 
 import { weekdays } from './utils/weekdays';
 
-  //// should display "kort översikt för veckan"
-  //// maybe just icons (and temperature?) then?
-
 function WeatherWeek({ dailyWeather, tempIsCelsius, getFahrenheitTemp }) {
-  const weatherDataWeekArr = dailyWeather.data;
-  weatherDataWeekArr.shift();
+  const weatherDataWeekArr = dailyWeather.data.slice(1, 8);
 
     return (
     <section className="weatherWeek">
-      <h3>the coming week:</h3>
+      <h3>coming week:</h3>
       <div className="weatherWeekOverview">
       {weatherDataWeekArr.map((day, index) => {
-        const CelsiusLow = day.temperatureLow.toFixed();
-        const CelsiusHigh = day.temperatureHigh.toFixed();
-        const FahrenheitLow = getFahrenheitTemp(day.temperatureLow).toFixed();
-        const FahrenheitHigh = getFahrenheitTemp(day.temperatureHigh).toFixed();
+        const celsiusHigh = day.temperatureHigh.toFixed();
+        const fahrenheitHigh = getFahrenheitTemp(day.temperatureHigh).toFixed();
         return (
           <div key={index}>
             <p>{weekdays[new Date(day.time*1000).getDay()]}</p>
-            <p>{new Date(day.time*1000).toLocaleDateString()}</p>
+            <p>{new Date(day.time*1000).toDateString().slice(4, 10)}</p>
             <p>{day.icon}</p>
             <p>{tempIsCelsius ? 
-              `${CelsiusLow} °C - ${CelsiusHigh} °C` : 
-              `${FahrenheitLow} °F - ${FahrenheitHigh} °F`}</p> 
+              celsiusHigh + ' °C' : 
+              fahrenheitHigh + ' °F'}</p> 
           </div>
         )
       })}
