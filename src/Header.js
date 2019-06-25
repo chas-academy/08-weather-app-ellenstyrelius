edea7e723d9a66d34d78dc71ec9e0775f1842d37
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-06-25 19:12:46 +0200

    Move all styling of header from index.css into the Header component by using styled-components.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Header() {
+const Background = styled.header`
+  background-image: linear-gradient(#679cda, #ff9900);
+  color: white;
+  text-align: center;
+  width: 100%;
+  padding: 80px 0;
+  margin-bottom: 60px;
+  position: relative;
+  top: 0;
+`
+const Title = styled.h1`
+  font-size: 90px;
+  font-weight: 100;
+  margin-bottom: 24px;
+`
+
+function Header() {
   return (
-    <header className="header">
-      <h1>rain or shine</h1>
+    <Background>
+      <Title>rain or shine</Title>
       <h2>weather forecaster</h2>
-    </header>
+    </Background>
   )
 }
+
+export default Header;
